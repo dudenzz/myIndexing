0f0aae7c7f861167a70328ff490e734080b1b0f0
Jakub Dutkiewicz <kubadt@gmail.com>
2016-08-30 12:27:20 +0200

    Index #1
    Added interface class
    Added simple flat wiki index class
    
    Temporarily removed interface connection
    
    Implementation of Dictionary
    
    Implementation of Save

--- Index/FlatWikiIndex.py
+++ Index/FlatWikiIndex.py
@@ -5,6 +5,7 @@ class FlatWikiIndex():
         self.Dictionary = {}
         self.Documents = []
         self.Frequencies = {}
+        self.InverseIndex = {}
     def createIndex(self, foldername):
         fObject = open(foldername)
         iterator = 0
@@ -15,15 +16,17 @@ class FlatWikiIndex():
                 if not self.Frequencies.__contains__(w):
                     self.Frequencies[w] = 1
                     iterator +=1
-                    if iterator%10000 == 0:
+                    if iterator%100000 == 0:
                         print str(iterator) + ' parsed words (2016 wiki contains 8392514 words)'
                 else:
                     self.Frequencies[w] += 1
                     if self.Frequencies[w] == 5:
                         self.Dictionary[w] = iterator2
+                        self.InverseIndex[iterator2] = []
                         iterator2 +=1
                         if iterator2%10000 == 0:
                             print str(iterator2) + ' entries'
+
 
         lght =  len(self.Dictionary)
         print '... created dictionary with ' + str(lght) + ' entries.'
@@ -32,21 +35,36 @@ class FlatWikiIndex():
         for i,l in enumerate(fObject):
             self.Documents.append({})
             if i%10000 == 0:
-                print str(i) + ' documents'
+                print str(i) + ' documents (wiki 2016 = 3977901 documents)'
             for w in l.split():
                 if self.Dictionary.__contains__(w):
+                    if not self.InverseIndex[self.Dictionary[w]].__contains__(i):
+                        self.InverseIndex[self.Dictionary[w]].append(i)
                     if not self.Documents[i].__contains__(self.Dictionary[w]):
                         self.Documents[i][self.Dictionary[w]] = 1
                     else:
                         self.Documents[i][self.Dictionary[w]] += 1
         print 'Successfully created an index'
 
-    def saveIndex(self, filename):
-        fObject = (filename)
-
+    def saveIndex(self, fDObject, fFObject, fIndObject, fInvIndObject):
+        for w in self.Dictionary:
+            fDObject.write(str(w) + ' ' + str(self.Dictionary[w]) + '\n')
+        for f in self.Frequencies:
+            fFObject.write(str(f) + ' ' + str(self.Frequencies[f]) + '\n')
+        for file in self.Documents:
+            fIndObject.write(str(file) + '\n')
+        for word in self.InverseIndex:
+            fInvIndObject.write('{' + str(word) +': ' + str(self.InverseIndex[word]) + '}' + '\n')
 
 index = FlatWikiIndex()
 index.createIndex('sample file')
 print index.Documents
 print index.Dictionary
 print index.Frequencies
+print index.InverseIndex
+
+fdo = open('dict','w+')
+ffo = open('freq','w+')
+fio = open('index','w+')
+fiio = open('inverse','w+')
+index.saveIndex(fdo,ffo,fio,fiio)
